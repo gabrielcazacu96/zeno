21f62abd75e960e455c42fe8cad15abdf597dcca
Mathieu Urstein <mathieu.urstein@gmail.com>
2025-02-08 17:50:44 +0100

    Start the real documentation

---
+++ apps/documentation/next.config.ts
@@ -0,0 +1,13 @@
+import type { NextConfig } from "next"
+
+import nextra from "nextra"
+
+const withNextra = nextra({
+  // Nextra config options
+})
+
+const nextConfig: NextConfig = withNextra({
+  // Next.js config options
+})
+
+export default nextConfig
