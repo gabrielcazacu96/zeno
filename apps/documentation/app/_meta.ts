a51daa29b668b35d90b179f15e28409f02c67519
Mathieu Urstein <mathieu.urstein@gmail.com>
2025-02-08 17:56:09 +0100

    Sidebar ordering

---
+++ apps/documentation/app/_meta.ts
@@ -0,0 +1,14 @@
+/* eslint-disable perfectionist/sort-objects */
+import type { MetaRecord } from "nextra"
+
+const meta: MetaRecord = {
+  index: {},
+  monorepo: {},
+  ui: {},
+  next: {},
+  documentation: {},
+  deployment: {},
+
+}
+
+export default meta
