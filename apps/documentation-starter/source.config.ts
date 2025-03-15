1b301e4828ff93ce6128d2908e86c3a35a64e15e
Mathieu Urstein <mathieu.urstein@gmail.com>
2025-03-15 20:03:37 +0100

    setup source for fomadocs

---
+++ apps/documentation-starter/source.config.ts
@@ -0,0 +1,11 @@
+import { defineDocs } from "fumadocs-mdx/config"
+
+export const source = defineDocs({
+  dir: "content",
+  docs: {
+    // options for `doc` collection
+  },
+  meta: {
+    // options for `meta` collection
+  },
+})
---
+++ apps/documentation/source.config.ts
@@ -0,0 +1,11 @@
+import { defineDocs } from "fumadocs-mdx/config"
+
+export const content = defineDocs({
+  dir: "content",
+  docs: {
+    // options for `doc` collection
+  },
+  meta: {
+    // options for `meta` collection
+  },
+})
