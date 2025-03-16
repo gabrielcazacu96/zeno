a2dd531a8e5ffefa8ca52da0f580523d0170d673
Mathieu Urstein <mathieu.urstein@gmail.com>
2025-03-16 16:40:29 +0100

    blog page

--- apps/www/lib/metadata.ts
+++ apps/www/lib/metadata.ts
@@ -6,3 +6,27 @@ export const metadataImage = createMetadataImage({
   imageRoute: "og",
   source,
 })
+
+import type { Metadata } from "next/types"
+
+export function createMetadata(override: Metadata): Metadata {
+  return {
+    ...override,
+    openGraph: {
+      description: override.description ?? undefined,
+      // images: "/banner.png",
+      siteName: "Zeno",
+      title: override.title ?? undefined,
+      url: "https://zeno.vercel.app",
+      ...override.openGraph,
+    },
+    twitter: {
+      card: "summary_large_image",
+      creator: "@money_is_shark",
+      description: override.description ?? undefined,
+      // images: "/banner.png",
+      title: override.title ?? undefined,
+      ...override.twitter,
+    },
+  }
+}
