7023ed83de48cabdc9baf045d7c3dd17847c80d2
Mathieu Urstein <mathieu.urstein@gmail.com>
2026-02-24 15:47:24 +0100

    Enhance docs tabs look

--- apps/docs/src/components/layout/header-tabs.tsx
+++ apps/docs/src/components/layout/header-tabs.tsx
@@ -1,74 +1,52 @@
 "use client"
 
+import { Tabs, TabsList, TabsTrigger } from "@zeno/ui/components/tabs"
 import { cn } from "@zeno/ui/lib/utils"
 import { Link, usePathname } from "fumadocs-core/framework"
 import { isTabActive } from "fumadocs-ui/components/sidebar/tabs/dropdown"
-import {
-  getSidebarTabs,
-  type SidebarTab,
-} from "fumadocs-ui/components/sidebar/tabs/index"
-import { type ComponentProps, useMemo } from "react"
+import { getSidebarTabs } from "fumadocs-ui/components/sidebar/tabs/index"
+import { useMemo } from "react"
 import { source } from "@/lib/source"
 
-function LayoutTabs({
-  options,
-  ...props
-}: ComponentProps<"div"> & {
-  options: SidebarTab[]
-}) {
-  const pathname = usePathname()
-  const selected = useMemo(() => {
-    return options.findLast((option) => isTabActive(option, pathname))
-  }, [options, pathname])
-
-  return (
-    <div
-      {...props}
-      className={cn(
-        "flex flex-row items-end gap-6 overflow-auto [grid-area:main]",
-        props.className
-      )}
-    >
-      {options.map((option, i) => (
-        <Link
-          className={cn(
-            "inline-flex items-center gap-1 text-nowrap border-transparent border-b-2 pb-1.5 font-medium text-fd-muted-foreground text-sm transition-colors hover:text-fd-accent-foreground [&_svg]:size-4!",
-            option.unlisted && selected !== option && "hidden",
-            selected === option && "border-fd-primary text-fd-primary"
-          )}
-          href={option.url}
-          // biome-ignore lint/suspicious/noArrayIndexKey: can't do better
-          key={i}
-        >
-          {option.icon}
-          {option.title}
-        </Link>
-      ))}
-    </div>
-  )
-}
-
-export function DocsLayoutHeaderTabs({
-  className,
-  ...props
-}: ComponentProps<"div">) {
+export function DocsLayoutHeaderTabs() {
   const tabs = useMemo(() => {
     return getSidebarTabs(source.getPageTree())
   }, [])
+  const pathname = usePathname()
+  const selected = useMemo(() => {
+    return tabs.findLast((tab) => isTabActive(tab, pathname))
+  }, [tabs, pathname])
 
   return (
     <div
-      className={cn(
-        "top-[60px] z-30 hidden *:mx-auto *:max-w-(--fd-layout-width) md:block",
-        className
-      )}
+      className="top-[62px] z-30 hidden *:mx-auto *:max-w-(--fd-layout-width) md:block"
       id="nd-docs-nav"
-      {...props}
     >
-      <LayoutTabs
-        className="z-10 border-b bg-fd-background px-4 pt-3 max-md:hidden"
-        options={tabs}
-      />
+      <div className="border-b bg-fd-background px-4 py-1.5">
+        <Tabs>
+          <TabsList>
+            {tabs.map((option, i) => (
+              <TabsTrigger
+                asChild
+                className={cn(
+                  "text-fd-muted-foreground [&_svg]:size-4!",
+                  option.unlisted && selected !== option && "hidden",
+                  selected === option && "border-fd-primary text-fd-primary",
+                  selected !== option && "hover:text-fd-accent-foreground"
+                )}
+                // biome-ignore lint/suspicious/noArrayIndexKey: can't do better
+                key={i}
+                value={option.url}
+              >
+                <Link href={option.url}>
+                  {option.icon}
+                  {option.title}
+                </Link>
+              </TabsTrigger>
+            ))}
+          </TabsList>
+        </Tabs>
+      </div>
     </div>
   )
 }
