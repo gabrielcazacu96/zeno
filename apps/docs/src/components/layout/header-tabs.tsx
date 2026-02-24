b941d8d71e54bbeb05657aae1b4b13f3470b1032
Mathieu Urstein <mathieu.urstein@gmail.com>
2026-02-24 13:03:04 +0100

    Docs Navigation tabs with styling and state management

--- apps/docs/src/components/layout/header-tabs.tsx
+++ apps/docs/src/components/layout/header-tabs.tsx
@@ -1,51 +1,73 @@
+"use client"
+
 import { cn } from "@zeno/ui/lib/utils"
-import { Link } from "fumadocs-core/framework"
-import { type Folder, getPageTreeRoots } from "fumadocs-core/page-tree"
-import { Tabs, TabsList, TabsTrigger } from "fumadocs-ui/components/tabs"
-import type { ComponentProps } from "react"
+import { Link, usePathname } from "fumadocs-core/framework"
+import { isTabActive } from "fumadocs-ui/components/sidebar/tabs/dropdown"
+import {
+  getSidebarTabs,
+  type SidebarTab,
+} from "fumadocs-ui/components/sidebar/tabs/index"
+import { type ComponentProps, useMemo } from "react"
 import { source } from "@/lib/source"
+
+function LayoutTabs({
+  options,
+  ...props
+}: ComponentProps<"div"> & {
+  options: SidebarTab[]
+}) {
+  const pathname = usePathname()
+  const selected = useMemo(() => {
+    return options.findLast((option) => isTabActive(option, pathname))
+  }, [options, pathname])
+
+  return (
+    <div
+      {...props}
+      className={cn(
+        "flex flex-row items-end gap-6 overflow-auto [grid-area:main]",
+        props.className
+      )}
+    >
+      {options.map((option, i) => (
+        <Link
+          className={cn(
+            "inline-flex items-center gap-2 text-nowrap border-transparent border-b-2 pb-1.5 font-medium text-fd-muted-foreground text-sm transition-colors hover:text-fd-accent-foreground",
+            option.unlisted && selected !== option && "hidden",
+            selected === option && "border-fd-primary text-fd-primary"
+          )}
+          href={option.url}
+          // biome-ignore lint/suspicious/noArrayIndexKey: can't do better
+          key={i}
+        >
+          {option.title}
+        </Link>
+      ))}
+    </div>
+  )
+}
 
 export function DocsLayoutHeaderTabs({
   className,
   ...props
 }: ComponentProps<"div">) {
-  const items = getPageTreeRoots(source.getPageTree())
-  const folderItems = items.filter(
-    (item) => !!item && "type" in item && item.type === "folder"
-  ) as Folder[]
-
-  //   console.log(folderItems)
+  const tabs = useMemo(() => {
+    return getSidebarTabs(source.getPageTree())
+  }, [])
 
   return (
     <div
       className={cn(
-        "top-[61px] z-30 hidden *:mx-auto *:max-w-(--fd-layout-width) md:block",
+        "top-[60px] z-30 hidden *:mx-auto *:max-w-(--fd-layout-width) md:block",
         className
       )}
       id="nd-docs-nav"
       {...props}
     >
-      <Tabs
-        className="my-0 rounded-none border-t-0 border-r-0 border-b border-l-none bg-background px-0 pt-1"
-        defaultIndex={0}
-      >
-        <TabsList>
-          {folderItems.map((item, index) => (
-            <Link
-              href={
-                item.children.filter((child) => child.type === "page")[0]
-                  ?.url ?? ""
-              }
-              key={item.$id ?? index.toString()}
-            >
-              <TabsTrigger value={item.$id ?? index.toString()}>
-                {item.icon}
-                {item.name}
-              </TabsTrigger>
-            </Link>
-          ))}
-        </TabsList>
-      </Tabs>
+      <LayoutTabs
+        className="z-10 border-b bg-fd-background px-4 pt-3 max-md:hidden"
+        options={tabs}
+      />
     </div>
   )
 }
