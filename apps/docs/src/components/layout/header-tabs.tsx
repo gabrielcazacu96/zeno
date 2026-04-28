6911dd935c720f6531dd8d3997d4bde81262d3a1
Mathieu Urstein <mathieu.urstein@gmail.com>
2026-04-28 10:53:24 +0200

    Update fumadocs

--- apps/docs/src/components/layout/header-tabs.tsx
+++ apps/docs/src/components/layout/header-tabs.tsx
@@ -3,8 +3,8 @@
 import { Tabs, TabsList, TabsTrigger } from "@zeno/ui/components/tabs"
 import { cn } from "@zeno/ui/lib/utils"
 import { Link, usePathname } from "fumadocs-core/framework"
+import { getSidebarTabs } from "fumadocs-ui/components/sidebar/tabs"
 import { isTabActive } from "fumadocs-ui/components/sidebar/tabs/dropdown"
-import { getSidebarTabs } from "fumadocs-ui/components/sidebar/tabs/index"
 import { useMemo } from "react"
 import { source } from "@/lib/source"
 
