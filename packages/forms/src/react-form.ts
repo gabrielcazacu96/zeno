c30014b9e393e1ef253a0b743616be2d820f9d4f
Mathieu Urstein <mathieu.urstein@gmail.com>
2026-05-05 10:20:28 +0200

    Give access to react-form through @zeno/forms

---
+++ packages/forms/src/react-form.ts
@@ -0,0 +1,4 @@
+/** biome-ignore-all lint/performance/noBarrelFile: TanStack Form re-export surface
+ *  Re-exports TanStack Form so consumers depend only on `@zeno/forms`.
+ */
+export * from "@tanstack/react-form"
